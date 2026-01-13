73cd6ede20e015e9fee7a4510db1e0ecee7d47c0
yyuh <dustinohlemann@gmail.com>
2026-01-13 20:33:07 +0100

    pain

--- commons/src/main/java/club/revived/commons/inventories/util/HeadBuilder.java
+++ commons/src/main/java/club/revived/commons/inventories/util/HeadBuilder.java
@@ -1,14 +1,21 @@
 package club.revived.commons.inventories.util;
 
+import com.destroystokyo.paper.profile.PlayerProfile;
 import com.destroystokyo.paper.profile.ProfileProperty;
 import io.papermc.paper.datacomponent.DataComponentTypes;
 import io.papermc.paper.datacomponent.item.ResolvableProfile;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerTextures;
 import org.jetbrains.annotations.NotNull;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.UUID;
 
 /**
  * This is an interesting Class
@@ -20,15 +27,23 @@ public final class HeadBuilder {
 
     @NotNull
     public static ItemStack customHead(final String url) {
-        final var head = ItemStack.of(Material.PLAYER_HEAD);
-        final var json = "{\"SKIN\":{\"url\":\"%s\"}}".formatted(url);
-        final var base64 = Base64.getEncoder().encodeToString(json.getBytes(StandardCharsets.UTF_8));
+        try {
+            final UUID uuid = UUID.randomUUID();
+            final PlayerProfile profile = Bukkit.createProfile(uuid);
 
-        head.setData(
-                DataComponentTypes.PROFILE,
-                ResolvableProfile.resolvableProfile().addProperty(new ProfileProperty("textures", base64))
-        );
+            PlayerTextures textures = profile.getTextures();
+            profile.setTextures(textures);
 
-        return head;
+            final ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+            final SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+            skullMeta.setPlayerProfile(profile);
+            textures.setSkin(URI.create(url).toURL());
+
+            head.setItemMeta(skullMeta);
+
+            return head;
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
