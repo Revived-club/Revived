572e7f00b0995721ebec0b288c0ec4cbc17944c2
yyuh <dustinohlemann@gmail.com>
2026-01-15 17:08:51 +0100

    debug

--- lobby/src/main/java/club/revived/lobby/game/friends/FriendManager.java
+++ lobby/src/main/java/club/revived/lobby/game/friends/FriendManager.java
@@ -63,6 +63,8 @@ public final class FriendManager {
         final var networkPlayer = PlayerManager.getInstance().fromBukkitPlayer(uuid);
 
         networkPlayer.getCachedOrLoad(FriendHolder.class).thenAccept(friendHolder -> {
+            System.out.println("aeeWEEAWEAEAEaewaaewweew4raweqrwwawaaweaweaewaweaewaeweaw");
+
             if (friendHolder == null) {
                 friendHolder = new FriendHolder(uuid, new ArrayList<>());
             }
