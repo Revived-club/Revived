16b51265b687681c405439e9ed6d1d679651e96b
yyuh <dustinohlemann@gmail.com>
2026-01-17 14:10:58 +0100

    trying to figuere out my kits aren't saving properly

--- lobby/src/main/java/club/revived/lobby/game/inventory/KitTemplateEditorMenu.java
+++ lobby/src/main/java/club/revived/lobby/game/inventory/KitTemplateEditorMenu.java
@@ -24,60 +24,62 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public final class KitTemplateEditorMenu {
 
-    public KitTemplateEditorMenu(
-            final EditedKitTemplate template,
-            final Player player
-    ) {
-        final var menu = AbstractMenu.of(6, "Editing " + template.kitType().getBeautifiedName());
-        final var networkPlayer = PlayerManager.getInstance()
-                .fromBukkitPlayer(player);
+  public KitTemplateEditorMenu(
+      final EditedKitTemplate template,
+      final Player player) {
+    final var menu = AbstractMenu.of(6, "Editing " + template.kitType().getBeautifiedName());
+    final var networkPlayer = PlayerManager.getInstance()
+        .fromBukkitPlayer(player);
 
-        final var content = template.content();
-        for (int slot = 36; slot < 41; ++slot) {
-            menu.button(new AbstractButton(slot - 36, ItemBuilder.item(content.getOrDefault(slot, new ItemStack(Material.AIR))), event -> {
-            }));
-        }
-        for (int slot = 9; slot < 36; ++slot) {
-            menu.button(new AbstractButton(slot, ItemBuilder.item(content.getOrDefault(slot, new ItemStack(Material.AIR))), event -> {
-            }));
-        }
-        for (int slot = 0; slot < 9; slot++) {
-            menu.button(new AbstractButton(slot + 36, ItemBuilder.item(content.getOrDefault(slot, new ItemStack(Material.AIR))), event -> {
-            }));
-        }
-
-        menu.onClose(event -> {
-            networkPlayer.getCachedOrLoad(EditedKitHolder.class).thenAccept(editedKitHolder -> {
-                final Map<Integer, ItemStack> contents = new ConcurrentHashMap<>();
-
-                for (int slot = 0; slot < 5; slot++) {
-                    final ItemStack item = event.getInventory().getItem(slot);
-                    contents.put(slot + 36, Objects.requireNonNullElseGet(item, () -> new ItemStack(Material.AIR)));
-                }
-
-                for (int slot = 9; slot < 36; slot++) {
-                    final ItemStack item = event.getInventory().getItem(slot);
-                    contents.put(slot, Objects.requireNonNullElseGet(item, () -> new ItemStack(Material.AIR)));
-                }
-
-                for (int slot = 36; slot < 45; slot++) {
-                    final ItemStack item = event.getInventory().getItem(slot);
-                    contents.put(slot - 36, Objects.requireNonNullElseGet(item, () -> new ItemStack(Material.AIR)));
-                }
-
-                final var kit = new EditedKitTemplate(
-                        player.getUniqueId(),
-                        template.kitType(),
-                        contents
-                );
-
-                editedKitHolder.kits().put(template.kitType(), kit);
-
-                networkPlayer.cacheValue(EditedKitHolder.class, editedKitHolder);
-                DatabaseManager.getInstance().save(EditedKitHolder.class, editedKitHolder);
-            });
-        });
-
-        menu.open(player);
+    final var content = template.content();
+    for (int slot = 36; slot < 41; ++slot) {
+      menu.button(new AbstractButton(slot - 36,
+          ItemBuilder.item(content.getOrDefault(slot, new ItemStack(Material.AIR))), event -> {
+          }));
     }
+    for (int slot = 9; slot < 36; ++slot) {
+      menu.button(
+          new AbstractButton(slot, ItemBuilder.item(content.getOrDefault(slot, new ItemStack(Material.AIR))), event -> {
+          }));
+    }
+    for (int slot = 0; slot < 9; slot++) {
+      menu.button(new AbstractButton(slot + 36,
+          ItemBuilder.item(content.getOrDefault(slot, new ItemStack(Material.AIR))), event -> {
+          }));
+    }
+
+    menu.onClose(event -> {
+      networkPlayer.getCachedOrLoad(EditedKitHolder.class).thenAccept(editedKitHolder -> {
+        final Map<Integer, ItemStack> contents = new ConcurrentHashMap<>();
+
+        for (int slot = 0; slot < 5; slot++) {
+          final ItemStack item = event.getInventory().getItem(slot);
+          contents.put(slot + 36, Objects.requireNonNullElseGet(item, () -> new ItemStack(Material.AIR)));
+        }
+
+        for (int slot = 9; slot < 36; slot++) {
+          final ItemStack item = event.getInventory().getItem(slot);
+          contents.put(slot, Objects.requireNonNullElseGet(item, () -> new ItemStack(Material.AIR)));
+        }
+
+        for (int slot = 36; slot < 45; slot++) {
+          final ItemStack item = event.getInventory().getItem(slot);
+          contents.put(slot - 36, Objects.requireNonNullElseGet(item, () -> new ItemStack(Material.AIR)));
+        }
+
+        final var kit = new EditedKitTemplate(
+            player.getUniqueId(),
+            template.kitType(),
+            contents);
+
+        editedKitHolder.kits().put(template.kitType(), kit);
+
+        networkPlayer.sendMessage("Your kit is being saved");
+        networkPlayer.cacheValue(EditedKitHolder.class, editedKitHolder);
+        DatabaseManager.getInstance().save(EditedKitHolder.class, editedKitHolder);
+      });
+    });
+
+    menu.open(player);
+  }
 }
